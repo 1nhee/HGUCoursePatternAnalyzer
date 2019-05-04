90ea8b83f44a2e84e45e75b06553cd501893b05e
1nhee <21800025@handong.edu>
2019-05-04 15:42:07 +0900

    complete with explanation note

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -57,15 +57,23 @@ public class HGUCoursePatternAnalyzer {
 		
 		// TODO: implement this method
 		
+		//you need to create instances of students which is student class array
 		students = new Student[numOfStudents];
 		int j = 0;
 		
+		//you need to count it till the end of lines
 		for(int i = 0; i < 12; i++) {
+			//make a string instance to create the 'check_student' instance
         	String getNamesWithTrim = new String(lines[i].split(",")[1]);
+        	//make a instance to check it is already in students
         	Student check_student = new Student(getNamesWithTrim.trim());
+        	//if there is no same thing in the students
         	if(!(studentExist(students, check_student))) {
+        		//insert check_student in students
         		students[j] = check_student;
+        		//and update the count
         		j++;
+        		//but count shouldn't be over numOfStudents
         		if (j >= numOfStudents) {
           			 break;
           		 }
@@ -83,13 +91,18 @@ public class HGUCoursePatternAnalyzer {
 	private boolean studentExist(Student[] students, Student student) {
 		
 		// TODO: implement this method
+		
+		//you can't count it over numOfStudents
 		for(int i = 0; i < numOfStudents; i++) {
+			//if students[i] is empty(null), return false to fill it
 			if(students[i] == null) {
 				return false;
+			//if student's name already exist, return true to skip this student's name
 			}else if(students[i].getName().equals(student.getName())) {
 				return true;
 			}
 		}
+		//if student's name is not in students yet, return false to insert this student's name
 		return false;
 	}
 	
@@ -102,15 +115,22 @@ public class HGUCoursePatternAnalyzer {
 		
 		// TODO: implement this method
 		
+		//you need to create instances of courses which is course class array
 		courses = new Course[numOfCourses]; 
 		int j = 0;
 		
+		//you need to count it till the end of lines
         for(int i = 0; i < 12; i++) {
+        	//make a string instance to create the 'check_course' instance
         	 String getNamesWithTrim = new String(lines[i].split(",")[2]);
         	 Course check_course = new Course(getNamesWithTrim.trim());
+        	//if there is no same thing in the courses
         	 if(!(courseExist(courses, check_course))) {
+        		//insert check_course in courses
         		 courses[j] = check_course;
+        		//and update the count
         		 j++;
+        		//but the count shouldn't be over numOfCourses
         		 if (j >= numOfCourses) {
         			 break;
         		 }
@@ -128,13 +148,18 @@ public class HGUCoursePatternAnalyzer {
 	private boolean courseExist(Course[] courses, Course course) {
 		
 		// TODO: implement this method
+		
+		//you can't count it over numOfStudents
 		for(int i = 0; i < numOfCourses; i++){
+			//if courses[i] is empty(null), return false to fill it
 			if(courses[i] == null) {
 				return false;
+			//if course's name already exist, return true to skip this course name
 			}else if(courses[i].getCourseName().equals(course.getCourseName())) {
 				return true;
 			}
 		}
+		//if course's name is not in courses yet, return false to insert this course's name
 		return false;
 	}
 
